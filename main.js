87abf1c8e62d605a049e45f7380bc5956af1f224
Maria Eduarda <mariaeduarda.art18@gmail.com>
2024-10-15 08:49:30 -0300

    Implementando as funcionalidades nos botões

--- main.js
+++ main.js
@@ -9,13 +9,16 @@ let contador = 0;
 
 //estrutura de repetição while = enquanto
 while (contador < listaDeTeclas.length) {
-    //criando uma função anônima
-    //As funções anônimas úteis quando queremos executar alguma coisa uma única vez ou em apenas um lugar. 
-    listaDeTeclas[contador].onclick = function() {
-        tocaSom('#som_tecla_pom');
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    //criando uma função anônima, as funções anônimas são úteis quando queremos executar alguma coisa uma única vez ou em apenas um lugar. 
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
     //contador = contador + 1;
     contador++;
-
-    console.log(contador);
 }
