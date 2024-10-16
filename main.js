4125445465a4e7aa50b3976c161c2d5bc0e9bc7f
Maria Eduarda <mariaeduarda.art18@gmail.com>
2024-10-16 10:57:54 -0300

    Alteração no if

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function tocaSom (seletorAudio) {
    const elemento = document.querySelector(seletorAudio);
 
-   if (elemento != null && elemento.localName === 'audio') {
+   if (elemento && elemento.localName === 'audio') {
         elemento.play()       
    } 
    else {
